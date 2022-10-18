8a6de4343e74d7da16cfc2e0b05ec613c31fc238
supriyosamanta1997 <supriyosamanta1997@gmail.com>
2022-10-18 15:32:48 +0530

    done

--- unit -2/Vowels in Rows.js
+++ unit -2/Vowels in Rows.js
@@ -1,50 +1,50 @@
-Description
+// Description
 
-You are given a 2D array, whose dimensions are stored in two variables with the nameNandM
+// You are given a 2D array, whose dimensions are stored in two variables with the nameNandM
 
-The value stored inNdenotes the number of rows, and the value inMdenotes the number of columns
+// The value stored inNdenotes the number of rows, and the value inMdenotes the number of columns
 
-The 2D array is stored in a variable with the namearr, and contains lower case English characters
+// The 2D array is stored in a variable with the namearr, and contains lower case English characters
 
-You have to printYes, if any particular row of a 2D array contains at least one vowel, at least printNo
+// You have to printYes, if any particular row of a 2D array contains at least one vowel, at least printNo
 
-For example, consider the value stored inN = 3,M = 2, and the array isarr = [a,b],[c,d],[e,f], then the required output will be
+// For example, consider the value stored inN = 3,M = 2, and the array isarr = [a,b],[c,d],[e,f], then the required output will be
 
-Yes (This row contains the vowel a)
-No  (This row does not contains any vowel)
-Yes (This row contains the vowel e)
-
-
-Input
-The first line of the input contains the value stored inNandM
-
-The nextNlines containMvalues each denoting the value stored inarr
+// Yes (This row contains the vowel a)
+// No  (This row does not contains any vowel)
+// Yes (This row contains the vowel e)
 
 
-Output
-For each row, printYes, if the row contains at least one vowel, otherwise printNo
+// Input
+// The first line of the input contains the value stored inNandM
 
-Sample Input 1 
-
-3 2
-a b
-c d
-e f
-Sample Output 1
-
-Yes
-No
-Yes
-Hint
-
-In the sample test case, the value stored inN = 3,M = 2, and the array isarr = [a,b],[c,d],[e,f], then the required output will be
-
-Yes (This row contains the vowel a)
-No  (This row does not contains any vowel)
-Yes (This row contains the vowel e)
+// The nextNlines containMvalues each denoting the value stored inarr
 
 
- sol:
+// Output
+// For each row, printYes, if the row contains at least one vowel, otherwise printNo
+
+// Sample Input 1 
+
+// 3 2
+// a b
+// c d
+// e f
+// Sample Output 1
+
+// Yes
+// No
+// Yes
+// Hint
+
+// In the sample test case, the value stored inN = 3,M = 2, and the array isarr = [a,b],[c,d],[e,f], then the required output will be
+
+// Yes (This row contains the vowel a)
+// No  (This row does not contains any vowel)
+// Yes (This row contains the vowel e)
+
+
+//  sol:
 
  function vowelsInRows(N,M,arr){
     //write code here
