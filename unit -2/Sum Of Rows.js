be7975adcc541b4322eea8217e13b135f6a9772f
supriyosamanta1997 <supriyosamanta1997@gmail.com>
2022-10-18 15:02:10 +0530

    done

--- unit -2/Sum Of Rows.js
+++ unit -2/Sum Of Rows.js
@@ -1,48 +1,48 @@
-Description
+// Description
 
-You are given a 2D array, whose dimensions are stored in two variables with the nameNandM
+// You are given a 2D array, whose dimensions are stored in two variables with the nameNandM
 
-The value stored inNdenotes the number of rows, and the value inMdenotes the number of columns
+// The value stored inNdenotes the number of rows, and the value inMdenotes the number of columns
 
-The 2D array is stored in a variable with the namearr
+// The 2D array is stored in a variable with the namearr
 
-You have to print the sum of all elements in a row, on a new line
+// You have to print the sum of all elements in a row, on a new line
 
-For example, consider the value stored inN = 3,M = 2, and the array isarr = [1,2],[3,4],[5,6], then the required output will be
+// For example, consider the value stored inN = 3,M = 2, and the array isarr = [1,2],[3,4],[5,6], then the required output will be
 
-3 (Adding the elements of the row [1,2])
-7 (Adding the elements of the row [3,4])
-11(Adding the elements of the row [5,6])
+// 3 (Adding the elements of the row [1,2])
+// 7 (Adding the elements of the row [3,4])
+// 11(Adding the elements of the row [5,6])
 
-Input
-The first line of the input contains the value stored inNandM
+// Input
+// The first line of the input contains the value stored inNandM
 
-The nextNlines containMvalues each denoting the value stored inarr
+// The nextNlines containMvalues each denoting the value stored inarr
 
 
-Output
-Print the sum of each row, on a new line. You have to do this for all rows of the matrix
+// Output
+// Print the sum of each row, on a new line. You have to do this for all rows of the matrix
 
-Sample Input 1 
+// Sample Input 1 
 
-3 2
-1 2 
-3 4
-5 6
-Sample Output 1
+// 3 2
+// 1 2 
+// 3 4
+// 5 6
+// Sample Output 1
 
-3
-7
-11
-Hint
+// 3
+// 7
+// 11
+// Hint
 
-In the sample test case, the value stored inN = 3,M = 2, and the array isarr = [1,2],[3,4],[5,6], then the required output will be
+// In the sample test case, the value stored inN = 3,M = 2, and the array isarr = [1,2],[3,4],[5,6], then the required output will be
 
-3 (Adding the elements of the row [1,2])
-7 (Adding the elements of the row [3,4])
-11(Adding the elements of the row [5,6])
+// 3 (Adding the elements of the row [1,2])
+// 7 (Adding the elements of the row [3,4])
+// 11(Adding the elements of the row [5,6])
 
-sol:
+// sol:
 
 
 function sumOfRows(N,M,arr){
